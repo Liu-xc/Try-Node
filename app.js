d7b97a0ba4fa8deb4a4cbf2694236ddb1131a665
Liu-xc <1392166232@qq.com>
2020-07-10 23:26:30 +0800

    finished (login, regist, delete, update) functions

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const router = require('./router')
 const path = require('path')
+const bodyParser = require('body-parser')
 
+// 使用express
 const app = express()
 const port = 3000
 
+// 使用bodyParser来解析请求的参数
+app.use(bodyParser.json())
+// 重要！
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 开放文件
 app.use('/public',express.static(path.join(__dirname, 'public')))
 
+// 指定渲染模板为html文件
 app.engine('html', require('express-art-template'))
 app.set('view options', {
     debug: process.env.NODE_ENV !== 'production'
 });
 
+// 注册路由，一定要放在所有中间件的最后
 app.use(router)
 
+// 监听端口
 app.listen(port, ()=>{
   console.log(`server is running on http://127.0.0.1:${port}...`)
 })
--- router.js
+++ router.js
@@ -1,51 +1,39 @@
 const express = require('express')
-const fs = require('fs')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/myapp');
+// 该变量保存当前用户用户名
+var curUsername = ''
+
+mongoose.connect('mongodb://localhost/myapp', {useNewUrlParser: true, useUnifiedTopology: true});
 const Schema = mongoose.Schema
-const blogSchema = new Schema({
-  name: String,
-  age: Number
+const userSchema = new Schema({
+  username: String,
+  password: String
 })
-const Blog = mongoose.model('Blog', blogSchema)
+const commentScheme = new Schema({
+  username: String,
+  comment: String,
+  time: Number
+})
+const User = mongoose.model('User', userSchema)
+const Comment = mongoose.model('Comment', commentScheme)
 
 const router = express.Router()
 
-router.get('/comment', (req, res)=>{
-  res.render('comment.html', {})
-  // let blog = new Blog({name: 'hello', age: 18})
-  // Blog.find({name: 'hello', age: 18}, (err, data)=>{
-  //   if (data) {
-  //     console.log(data)
-  //     console.log('数据已存在')
-  //     return res.end()
-  //   }
-  //   blog.save((err, data)=>{
-  //     if(err){
-  //       return res.status(500).end('ERROR')
-  //     } else {
-  //       console.log('保存数据' + data)
-  //     }
-  //   })
-  // })
-  res.end()
-})
-
-router.get('/a', (req, res)=>{
-  // Blog.find((err, data)=>{
-  //   if(err){
-  //     return res.status(500).send('ERROR')
-  //   } else {
-  //     console.log(`data: ${data}`)
-  //   }
-  // })
-  res.end()
-})
-
 router.get('/', (req, res)=>{
-  res.render('index.html', {})
+  // 从数据库获取评论
+  const commentArr = []
+  let failMSg = ''
+  Comment.find((err, data)=>{
+    if(err){
+      failMsg = '获取数据失败'
+    } else if (data) {
+      data.forEach(v=>commentArr.unshift(v))
+      res.render('index.html', {'username': curUsername, 'failMSg': failMSg, 'commentArr': commentArr})
+    }
+  })
 })
+
 
 router.get('/login', (req, res)=>{
   res.render('login.html', {
@@ -58,6 +46,132 @@ router.get('/register', (req, res)=>{
   res.render('register.html', {
     username: 'hello',
     password: 'world'
+  })
+})
+
+router.get('/comment', (req, res)=>{
+  res.render('comment.html', {'title': 'comment'})
+  res.end()
+})
+
+router.get('/delete', (req, res)=>{
+  // 获取id，从参数中获取的id=“xxxxx”，需要消去两边的引号才能查询
+  const id = req.query.id.slice(1, -1)
+  Comment.findByIdAndRemove(id, (err)=>{
+    if(err){
+      console.log(err)
+      res.render('index.html', {'failMSg': '删除失败'})
+    } else {
+      res.redirect('/')
+    }
+  })
+})
+
+// edit和comment使用同一个模板，通过传入的参数不同来控制页面的表现
+router.get('/edit', (req, res)=>{
+  const id = req.query.id.slice(1, -1)
+  Comment.findById(id, (err, data)=>{
+    if(err){
+      console.log(err)
+      res.render('comment.html', {'title': 'edit', 'failMsg': '获取数据失败', id})
+    } else if (data) {
+      res.render('comment.html', {'title': 'edit', 'comment': data.comment, id})
+    } else {
+      res.render('comment.html', {'title': 'edit', id})
+    }
+  })
+})
+
+router.post('/login', (req, res)=>{
+  // 获取数据，取得的数据的类型为object
+  const userData = req.body
+  
+  const promise = new Promise((resolve, reject)=>{
+    // 查询数据库，如果不存在数据就返回登录界面
+    // 如果存在数据就保留数据并进入登录状态的首页
+    User.findOne(userData, (err, data)=>{
+      if(err){
+        reject(err)
+      }
+      resolve(data)
+    })
+  }).then(
+    resolve=>{
+      // 进入登录状态的首页
+      curUsername = userData.username
+      res.redirect('/')
+    },
+    reject=>{
+      // 打印错误信息并返回登录页面
+      console.log(reject.message)
+      res.render('login.html', {'failMsg': '信息错误'})
+    }
+  )
+})
+
+router.post('/register', (req, res)=>{
+  const userData = req.body
+
+  const promise = new Promise((resolve, reject)=>{
+    // 查询数据库
+    User.findOne({username: userData.username}, (err, data)=>{
+      if (err) {
+        reject(err)
+      } else if (data) {
+        // 如果有数据就返回注册页面提示用户已存在
+        res.render('register.html', {'failMsg': '用户名已存在'})
+      } else {
+        // 没有数据就将数据存入数据库并且保留以及跳转到登录状态的首页
+        const user = new User(userData)
+        user.save((err)=>{
+          if(err){
+            reject(err)
+          }
+          curUsername = userData.username
+          res.redirect('/')
+        })
+      }
+    })
+  }).catch(err=>{
+    console.log(err.message)
+    res.render('register.html', {'failMsg': '登录失败'})
+  })
+})
+
+router.post('/comment', (req, res)=>{
+  // 首先要判断当前是否已经登录了
+  if(!curUsername){
+    res.render('comment.html', {'failMsg': '请登录'})
+  }
+  // 获取数据
+  const commentData = req.body
+  commentData.username = curUsername
+  commentData.time = Date.now()
+
+  const comment = new Comment(commentData)
+
+  // 存入数据库
+  comment.save((err)=>{
+    if(err) {
+      console.log(err)
+      return res.render('comment.html', {'failMsg': '发表失败'})
+    }
+    res.redirect('/')
+  })
+})
+
+router.post('/edit', (req, res)=>{
+  // 获取数据
+  const comment = req.body.comment
+  const id = req.body.id
+  
+  // 查询并修改
+  Comment.findByIdAndUpdate(id, {comment}, (err)=>{
+    if(err){
+      console.log(err)
+      res.render('comment.html', {'title': 'edit', 'failMsg': '更新失败', id})
+    }
+    res.redirect('/')
   })
 })
 
